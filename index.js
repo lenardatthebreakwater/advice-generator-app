97ca4a098cb911a312693675f7390bf0b5ea2dc8
Lenard <energeticbowlofrice@gmail.com>
2024-03-08 16:46:47 +0800

    Added a simple loader while waiting for fetch

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 let getAdviceBtn = document.querySelector('#getAdviceBtn')
 let adviceID = document.querySelector('#adviceID')
 let advice = document.querySelector('#advice')
+let loader = document.querySelector('#loader')
 
-getAdviceBtn.addEventListener("click", async () => {
-    request = await fetch('https://api.adviceslip.com/advice')
-    requestBody = await request.json()
+function displayAdvice(requestBody) {
     adviceID.innerText = `Advice #${requestBody.slip.id}`
-    advice.innerText = `"${requestBody.slip.advice}"` 
+    advice.innerText = `"${requestBody.slip.advice}"`
+    loader.style.display = 'none'
+    advice.style.display = 'block'
+}
+
+getAdviceBtn.addEventListener('click', () => {
+    loader.style.display = 'block'
+    advice.style.display = 'none'
+    fetch('https://api.adviceslip.com/advice')
+    .then((request) =>
+        request.json()
+    )
+    .then((requestBody) => (
+        displayAdvice(requestBody)
+    ))
 })
